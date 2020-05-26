987160ef39c95698beb67766d3744193d2efb5e2
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-26 17:29:51 +0200

    add view director , adjust router routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,46 @@
-import Vue from "vue"
-import VueRouter from "vue-router
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home";
+import AddDog from "../views/AddDog";
+import DogList from "../views/DogList";
+import Profile from "../views/Profile";
+import SignIn from "../views/SignIn";
+import SignUp from "../views/SignUp";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/dog-listing",
+      name: "dog-list",
+      component: DogList,
+    },
+    {
+      path: "/add-dog",
+      name: "add-dog",
+      component: AddDog,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile,
+    },
+    {
+      path: "/sign-in",
+      name: "sign-in",
+      component: SignIn,
+    },
+    {
+      path: "/sign-up",
+      name: "sign-up",
+      component: SignUp,
+    },
+  ],
+});
---
+++ src/views/AddDog.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>The Add Dog Page</p>
+  </div>
+</template>

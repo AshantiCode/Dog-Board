94fdb175d627f41777d543b95f910897f1f35ff9
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-31 01:31:12 +0200

    create single dog component

---
+++ src/components/Dogs/SingleDog.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title primary-title>
+            <h4 class="primary--text">Roxy die schöne</h4>
+          </v-card-title>
+          <v-img :src="require('@/assets/dog-1.jpg')" height="400px"></v-img>
+          <v-card-text>she is e fluffy puffy dogs that is seeking for a Home! Dont wait to long!!</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>

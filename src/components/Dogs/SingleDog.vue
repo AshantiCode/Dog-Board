b8a7db011b9273d0cafc08294889e1f9f459fb02
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-01 14:29:35 +0200

    update single dog

--- src/components/Dogs/SingleDog.vue
+++ src/components/Dogs/SingleDog.vue
@@ -4,24 +4,28 @@
       <v-col cols="10" sm="6" offset-md="3">
         <v-card>
           <v-card-title primary-title>
-            <h4 class="primary--text">Roxy die schöne</h4>
+            <h4 class="primary--text">Hi, I am {{dogs.title}}</h4>
           </v-card-title>
-          <v-img :src="require('@/assets/dog-1.jpg')" height="400px"></v-img>
-          <v-card-subtitle class="third--text"
-            >4 years old | happy | friendly | beginner</v-card-subtitle
-          >
-          <v-card-text
-            >She is a fluffy puffy dogg that is seeking for a Home! Dont wait to
-            long!!</v-card-text
-          >
+          <v-img :src="dogs.imageUrl" height="400px"></v-img>
+          <v-card-subtitle class="third--text">I am {{dogs.age}} years old :D</v-card-subtitle>
+          <v-card-text>{{dogs.description}}</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="third white--text" class="pa-5"
-              >Save To Favorite</v-btn
-            >
+            <v-btn color="third white--text" class="pa-5">Save To Favorite</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  props: ["id"],
+  computed: {
+    dogs() {
+      return this.$store.getters.loadedDog(this.id);
+    }
+  }
+};
+</script>
